fe65236d81b26dedd6d53ade75d746cf43110218
Zac Bentley <zbbentley@gmail.com>
2015-10-15 17:47:47 -0400

    Formatted to please IDEA.

--- lib/rabbitlock/connection.py
+++ lib/rabbitlock/connection.py
@@ -1,45 +1,46 @@
 import pika
 
+
 class SingleChannelConnection(object):
-	def __init__(self, parameters):
-		self._channel = None
-		self.parameters = parameters
-		self.get_channel()
+    def __init__(self, parameters):
+        self._channel = None
+        self.parameters = parameters
+        self.get_channel()
 
-	def get_channel(self):
-		if self._channel is not None:
-			if not self._channel.is_open or not self.connection.is_open:
-				self.clear_channel()
-		if self._channel is None:
-			self.connection = pika.BlockingConnection(self.parameters)
-			self.set_channel(self.connection.channel())
-		return self._channel
-	
-	def set_channel(self, channel):
-		channel.confirm_delivery()
-		self._channel = channel
-		return channel
-	
-	def clear_channel(self):
-		self._channel = None
-		try:
-			self._channel.close()
-		except:
-			pass
+    def get_channel(self):
+        if self._channel is not None:
+            if not self._channel.is_open or not self.connection.is_open:
+                self.clear_channel()
+        if self._channel is None:
+            self.connection = pika.BlockingConnection(self.parameters)
+            self.set_channel(self.connection.channel())
+        return self._channel
 
-		try:
-			self.connection.close()
-		except pika.exceptions.ConnectionClosed as e:
-			if e.args[0] != 302: # CONNECTION_FORCED
-				raise
+    def set_channel(self, channel):
+        channel.confirm_delivery()
+        self._channel = channel
+        return channel
 
-	channel = property(get_channel, set_channel, clear_channel)
+    def clear_channel(self):
+        self._channel = None
+        try:
+            self._channel.close()
+        except:
+            pass
 
-	def publish(self, *args, **kw):
-		return self.channel.basic_publish(*args, **kw)
+        try:
+            self.connection.close()
+        except pika.exceptions.ConnectionClosed as e:
+            if e.args[0] != 302:  # CONNECTION_FORCED
+                raise
 
-	def declare_queue(self, *args, **kw):
-		return self.channel.queue_declare(*args, **kw)
+    channel = property(get_channel, set_channel, clear_channel)
 
-	def delete_queue(self, *args, **kw):
-		return self.channel.queue_delete(*args, **kw)
+    def publish(self, *args, **kw):
+        return self.channel.basic_publish(*args, **kw)
+
+    def declare_queue(self, *args, **kw):
+        return self.channel.queue_declare(*args, **kw)
+
+    def delete_queue(self, *args, **kw):
+        return self.channel.queue_delete(*args, **kw)
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-import sys
-sys.path.append("lib")
-
 
 from cluster_utils import get_connection_parameters
-from rabbitlock.mutex import Mutex
 from time import sleep
+import sys
+
+sys.path.append("lib")
+from rabbitlock.mutex import Mutex
 
 conn = Mutex("foo", get_connection_parameters())
 while conn.ensure_acquired():
-	print("Got lock")
-	sleep(1)
+    print("Got lock")
+    sleep(1)
